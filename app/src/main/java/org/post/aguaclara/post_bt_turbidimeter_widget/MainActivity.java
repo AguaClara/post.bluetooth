a3fb827433b338fed5d74fe42aabc544b94731a9
danelimjoco <mdl259@cornell.edu>
2016-11-30 17:30:54 -0500

    Removed refresh button and repeatedly call getDoubleFromString() so that whenever user hits read on the turbidimeter, the NTU will change on the app. Also, closeBT() occurs whenever app is in background and openBT() occurs whenever app is reopened so that the threads aren't messed up anymore.

--- app/src/main/java/org/post/aguaclara/post_bt_turbidimeter_widget/MainActivity.java
+++ app/src/main/java/org/post/aguaclara/post_bt_turbidimeter_widget/MainActivity.java
@@ -9,6 +9,7 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Gravity;
@@ -19,11 +20,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.lang.Double;
 
 //import org.json.simple.JSONArray;
 //import org.json.simple.JSONObject;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 
@@ -55,6 +62,11 @@ public class MainActivity extends Activity {
    int counter;
    volatile boolean stopWorker;
    Intent incomingIntent;
+   /**
+    * ATTENTION: This was auto-generated to implement the App Indexing API.
+    * See https://g.co/AppIndexing/AndroidStudio for more information.
+    */
+   private GoogleApiClient client;
 
    @Override
    public void onCreate(Bundle savedInstanceState) {
@@ -72,7 +84,7 @@ public class MainActivity extends Activity {
 //         //      System.out.println(mAnswer);
 //         //     System.out.println(intent.getExtras().get("rawWaterTurbidity"));
 //         finish();
-         
+
 
       }
 
@@ -80,7 +92,7 @@ public class MainActivity extends Activity {
       Button openButton = (Button) findViewById(R.id.open);
       Button sendButton = (Button) findViewById(R.id.send);
       //Button closeButton = (Button) findViewById(R.id.close);
-      Button testButton = (Button) findViewById(R.id.test);
+      //Button testButton = (Button) findViewById(R.id.test);
       Button acceptButton = (Button) findViewById(R.id.accept);
       myLabel = (TextView) findViewById(R.id.label);
       turbidityLabel = (TextView) findViewById(R.id.turbidity);
@@ -100,7 +112,7 @@ public class MainActivity extends Activity {
       final AlertDialog dialog = builder.create();
 
 
-         //Error message for send button
+      //Error message for send button
       AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
       builder2.setMessage("You can't send now")
               .setTitle("Error!");
@@ -110,7 +122,6 @@ public class MainActivity extends Activity {
          }
       });
       final AlertDialog dialog2 = builder2.create();
-
 
 
       //Open Button
@@ -155,15 +166,15 @@ public class MainActivity extends Activity {
       });
 
       //Test Button
-      testButton.setOnClickListener(new View.OnClickListener() {
-         public void onClick(View v) {
-            try {
-               test();
-            } catch (Exception ex) {
-               System.out.println(ex);
-            }
-         }
-      });
+//      testButton.setOnClickListener(new View.OnClickListener() {
+//         public void onClick(View v) {
+//            try {
+//               test();
+//            } catch (Exception ex) {
+//               System.out.println(ex);
+//            }
+//         }
+//      });
 
       //Accept (and close) Button
       acceptButton.setOnClickListener(new View.OnClickListener() {
@@ -188,6 +199,9 @@ public class MainActivity extends Activity {
 //      });
 
 
+      // ATTENTION: This was auto-generated to implement the App Indexing API.
+      // See https://g.co/AppIndexing/AndroidStudio for more information.
+      client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
    }
 
    void test() {
@@ -201,8 +215,28 @@ public class MainActivity extends Activity {
 //      turbidityLabel.setText(finalresult);
 
       //Actual stuff (change name of this button to "Refresh")
-     // beginListenForData();
-      getDoubleFromString(allOutput);
+      // beginListenForData();
+
+      //A handler allows user to repeatedly hit read on the turbidimeter and the value will update
+//      final Handler h = new Handler();
+//      final int delay = 1000; //milliseconds
+//
+//      h.postDelayed(new Runnable(){
+//         public void run(){
+//            getDoubleFromString(allOutput);
+//            h.postDelayed(this, delay);
+//         }
+//      }, delay);
+
+      final Handler h = new Handler();
+      final int delay = 1000; //milliseconds
+
+      h.postDelayed(new Runnable() {
+         public void run() {
+            getDoubleFromString(allOutput);
+            h.postDelayed(this, delay);
+         }
+      }, delay);
 
 
    }
@@ -238,6 +272,11 @@ public class MainActivity extends Activity {
       mmInputStream = mmSocket.getInputStream();
 
       beginListenForData();
+      try {
+         test();
+      } catch (Exception ex) {
+         System.out.println(ex);
+      }
 
       myLabel.setText(getResources().getString(R.string.bluetooth_connected));
       turbidityLabel.setText("");
@@ -276,7 +315,11 @@ public class MainActivity extends Activity {
                            //CHANGE: Sends the allOutput String to my method
 
                            getDoubleFromString(allOutput);
+
+
                            readBufferPosition = 0;
+
+
                         } else {
                            readBuffer[readBufferPosition++] = b;
                         }
@@ -313,7 +356,7 @@ public class MainActivity extends Activity {
 
       //Only including this closeBT() will allow end user to refresh without having to reopen BT
       //but greater chance of error if the user closes the app without closing BT.
-      //closeBT();
+      closeBT();
       finish();
 
 
@@ -342,7 +385,8 @@ public class MainActivity extends Activity {
       mmOutputStream.close();
       mmInputStream.close();
       mmSocket.close();
-      myLabel.setText("Press OPEN to get another reading.\n\nPress SEND if finished.");
+      // myLabel.setText("Press OPEN to get another reading.\n\nPress SEND if finished.");
+      myLabel.setText("Complete!");
 
    }
 
@@ -404,7 +448,7 @@ public class MainActivity extends Activity {
 
          System.out.println(obj.getJSONObject("rd").getJSONObject("tur").getJSONArray("d").getDouble(0));
          return obj.getJSONObject("rd").getJSONObject("tur").getJSONArray("d").getDouble(0);
-      } catch (org.json.JSONException e) {
+      } catch (JSONException e) {
          System.out.println(e);
          finish();
       }
@@ -414,26 +458,87 @@ public class MainActivity extends Activity {
 
    //This method will take the output (String) from the Turbidimeter, turn it into a JSON Object,
    //and send the double
-   public void getDoubleFromString(String output){
-      try{
+   public void getDoubleFromString(String output) {
+      try {
          JSONObject obj = new JSONObject(output);
 
          Double mAnswer = obj.getJSONObject("rd").getJSONObject("tur").getJSONArray("d").getDouble(0);
          String finalresult = new Double(mAnswer).toString();
          turbidityLabel.setText(finalresult);
          //this closeBT() closes worker each time. More work, but less chance of error.
-         closeBT();
+         //closeBT();
 
 
          // incomingIntent.putExtra("value", mAnswer);
          // setResult(RESULT_OK, incomingIntent);
          // closeBT();
          // finish();
-      }
-      catch(Exception e) {
+      } catch (Exception e) {
          System.out.println(e);
          //   finish();
       }
    }
 
+   //This code closes the thread every time the app is in background and restarts when reopened!te
+@Override
+   protected void onPause() {
+   super.onPause();
+      try {
+         closeBT();
+      } catch (Exception ex) {
+         System.out.println(ex);
+      }
+   }
+
+   protected void onResume() {
+      super.onResume();
+      try {
+         openBT();
+      } catch (Exception ex) {
+         System.out.println(ex);
+      }
+   }
+   @Override
+
+
+
+
+   public void onStart() {
+      super.onStart();
+
+      // ATTENTION: This was auto-generated to implement the App Indexing API.
+      // See https://g.co/AppIndexing/AndroidStudio for more information.
+      client.connect();
+      Action viewAction = Action.newAction(
+              Action.TYPE_VIEW, // TODO: choose an action type.
+              "Main Page", // TODO: Define a title for the content shown.
+              // TODO: If you have web page content that matches this app activity's content,
+              // make sure this auto-generated web page URL is correct.
+              // Otherwise, set the URL to null.
+              Uri.parse("http://host/path"),
+              // TODO: Make sure this auto-generated app URL is correct.
+              Uri.parse("android-app://org.post.aguaclara.post_bt_turbidimeter_widget/http/host/path")
+      );
+      AppIndex.AppIndexApi.start(client, viewAction);
+   }
+
+   @Override
+   public void onStop() {
+      super.onStop();
+
+      // ATTENTION: This was auto-generated to implement the App Indexing API.
+      // See https://g.co/AppIndexing/AndroidStudio for more information.
+      Action viewAction = Action.newAction(
+              Action.TYPE_VIEW, // TODO: choose an action type.
+              "Main Page", // TODO: Define a title for the content shown.
+              // TODO: If you have web page content that matches this app activity's content,
+              // make sure this auto-generated web page URL is correct.
+              // Otherwise, set the URL to null.
+              Uri.parse("http://host/path"),
+              // TODO: Make sure this auto-generated app URL is correct.
+              Uri.parse("android-app://org.post.aguaclara.post_bt_turbidimeter_widget/http/host/path")
+      );
+      AppIndex.AppIndexApi.end(client, viewAction);
+      client.disconnect();
+   }
 }
